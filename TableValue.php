62c0659a8c3c42af931b4924a07698f38de1f671
arburhan <burhan128454@outlook.com>
2024-12-22 12:01:05 +0600

    Add studentInfo table creation and value insertion scripts

---
+++ TableValue.php
@@ -0,0 +1,27 @@
+<?php
+$serverName = 'localhost';
+$userName = 'root';
+$userPassword = 'root';
+$dbName = 'finaldb'; // its created on dbcreation.php file
+
+$connection = mysqli_connect($serverName, $userName, $userPassword, $dbName);
+$valueAdd = "insert into studentInfo values(161, 'tom', 'cse', 2022),(162, 'shahajadi', 'eee', 2022);";
+
+if(!$connection){
+    die('connection failed'. mysqli_connect_error());
+}
+else{
+    if(mysqli_query($connection, $valueAdd)){
+        echo 'value added on table successfully';
+    }
+    else{
+        echo 'Error: ' . mysqli_error($connection);
+    }
+    
+}
+
+
+
+
+
+?>
--- createTable.php
+++ createTable.php
@@ -7,7 +7,7 @@ $dbName = 'finaldb'; // its created on dbcreation.php file
 $connection = mysqli_connect($serverName, $username, $userPassword, $dbName);
 $tableSQL = 'create table studentInfo(ID int, name varchar(300), dept varchar(50), session int)';
 if(!$connection){
-    die('connection failed or table creation failed'. mysqli_connect_error());
+    die('connection failed'. mysqli_connect_error());
 }
 else{
     if(mysqli_query($connection, $tableSQL)){
